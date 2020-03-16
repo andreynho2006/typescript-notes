afe4a4200f6aad5d076c0798c8dd11ea57a5638e
andreynho2006 <andreynho2006@gmail.com>
2020-03-16 17:11:21 +0200

    generics, simple generics, built-in generics, generic types, generic classes,

--- namespace-and-modules/app.js
+++ namespace-and-modules/app.js
@@ -45,3 +45,77 @@ var oldPerson = {
     }
 };
 console.log(oldPerson);
+// generisc
+// console.log('<< GENERICS >>');
+// simple generic
+function echo(data) {
+    return data;
+}
+console.log(echo('Max').length);
+console.log(echo(27));
+console.log(echo({ name: 'Max', gase: 34 }));
+// better generic way
+function betterEcho(data) {
+    return data;
+}
+console.log(betterEcho('Max').length);
+console.log(betterEcho(27));
+console.log(betterEcho({ name: 'Max', gase: 34 }));
+// built-in generic
+//array
+var testResult = [1.94, 2.33];
+testResult.push(34.7);
+//testResult.push('cinci');
+console.log(testResult);
+// arrays
+function printAll(args) {
+    args.forEach(function (element) { return console.log(element); });
+}
+printAll(['Apple', 'Banana']);
+// generic types
+var echo2 = betterEcho;
+console.log(echo2('Something'));
+// Generic classes
+var SimpleMath = /** @class */ (function () {
+    function SimpleMath() {
+    }
+    SimpleMath.prototype.calculate = function () {
+        return +this.baseValue * +this.multiplyValue;
+    };
+    return SimpleMath;
+}());
+var simpleMath = new SimpleMath();
+simpleMath.baseValue = "10";
+simpleMath.multiplyValue = 10;
+console.log(simpleMath.calculate());
+//Create a generic Map (an Object like an Array, but instead with Key-Value Pairs). The key will always be a string.
+// Let's keep it simple and only add the following methods to the Map:
+var MyMap = /** @class */ (function () {
+    function MyMap() {
+        this.map = {};
+    }
+    MyMap.prototype.setItem = function (key, item) {
+        this.map[key] = item;
+    };
+    MyMap.prototype.getItem = function (key) {
+        return this.map[key];
+    };
+    MyMap.prototype.clear = function () {
+        this.map = {};
+    };
+    MyMap.prototype.printMap = function () {
+        for (var key in this.map) {
+            console.log(key, this.map[key]);
+        }
+    };
+    return MyMap;
+}());
+//The map should be usable like shown below:
+var numberMap = new MyMap();
+numberMap.setItem('apples', 5);
+numberMap.setItem('bananas', 10);
+numberMap.printMap();
+var stringMap = new MyMap();
+stringMap.setItem('name', "Max");
+stringMap.setItem('age', "27");
+stringMap.printMap();
--- namespace-and-modules/app.ts
+++ namespace-and-modules/app.ts
@@ -80,3 +80,89 @@ const oldPerson: AgedPerson = {
   }
 };
 console.log(oldPerson);
+
+// generisc
+// console.log('<< GENERICS >>');
+
+// simple generic
+function echo(data: any) {
+  return data;
+}
+
+console.log(echo('Max').length);
+console.log(echo(27));
+console.log(echo({ name: 'Max', gase: 34 }));
+
+// better generic way
+function betterEcho<T>(data: T) {
+  return data;
+}
+console.log(betterEcho('Max').length);
+console.log(betterEcho(27));
+console.log(betterEcho({ name: 'Max', gase: 34 }));
+
+// built-in generic
+//array
+const testResult: Array<number> = [1.94, 2.33];
+testResult.push(34.7);
+//testResult.push('cinci');
+console.log(testResult);
+
+// arrays
+function printAll<T>(args: T[]) {
+  args.forEach(element => console.log(element));
+}
+printAll<string>(['Apple', 'Banana']);
+
+// generic types
+const echo2: <T>(data: T) => T = betterEcho;
+console.log(echo2<string>('Something'));
+
+// Generic classes
+class SimpleMath<T extends number | string, U extends number | string> {
+  baseValue: T;
+  multiplyValue U;
+  calculate() : number {
+    return +this.baseValue * +this.multiplyValue;
+  }
+}
+
+const simpleMath = new SimpleMath<string, number>();
+simpleMath.baseValue = "10";
+simpleMath.multiplyValue = 10;
+console.log(simpleMath.calculate());
+
+//Create a generic Map (an Object like an Array, but instead with Key-Value Pairs). The key will always be a string.
+
+// Let's keep it simple and only add the following methods to the Map:
+class MyMap<T> {
+    private map: {[key: string]: T} = {};
+
+    setItem(key: string, item: T) {
+        this.map[key] = item;
+    }
+    getItem(key: string) {
+        return this.map[key];
+    }
+    clear() {
+        this.map = {}
+    }
+    printMap() {
+        for(let key in this.map) {
+            console.log(key, this.map[key])
+        }
+    }
+}
+
+
+//The map should be usable like shown below:
+
+const numberMap = new MyMap<number>();
+numberMap.setItem('apples', 5);
+numberMap.setItem('bananas', 10);
+numberMap.printMap();
+
+const stringMap = new MyMap<string>();
+stringMap.setItem('name', "Max");
+stringMap.setItem('age', "27");
+stringMap.printMap();
